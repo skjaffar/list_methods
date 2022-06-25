2c3480611bdd8b9f9059f15ef963d84763faad44
skjaffar <107207838+skjaffar@users.noreply.github.com>
2022-06-24 23:45:28 -0700

    Add files via upload

---
+++ list_method.py
@@ -0,0 +1,32 @@
+# a='jaffar'
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# print(a[3])
+# print(a[4])
+# print(a[5])
+
+
+
+# a=['car','mobile']
+# print(type(a))
+# print(a[1])
+# print(a[0])
+
+# a='vinny'
+# print(a[-1])
+# print(a[-2])
+# print(a[-3])
+# print(a[-4])
+
+# c=[1,56,6,'munna','hai']
+# print(c[1])
+# print(c[3][3])
+# print(c[-1][2])
+# print((c[3][0]),(c[3][1]),(c[3][2]),(c[3][3]),(c[3][4]))
+
+# j=[1.3,3,78,'hai']
+# j[1]=8
+# j[-1]='GT'
+# print(j)
+
